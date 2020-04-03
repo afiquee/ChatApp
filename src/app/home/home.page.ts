22c4a88316f5f76f7ff6c48c1eff921d2514254b
afiquee <47377243+afiquee@users.noreply.github.com>
2020-04-04 03:58:51 +0800

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,64 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  message = '';
+  messages = [];
+  currentUser = '';
+
+  constructor(private socket: Socket, private toastCtrl: ToastController) {}
+
+  ngOnInit() {
+    this.socket.connect();
+
+    let name = `User-${new Date().getTime()}`;
+    this.currentUser = name;
+
+    this.socket.emit('set-name', name);
+
+    this.socket.fromEvent('users-changed').subscribe(data => {
+      console.log('got data: ',data);
+      let user = data['user'];
+      if(data['event'] == 'left') {
+        this.showToast(`User left: ${user}`);
+      } else {
+        this.showToast(`User joined: ${user}`);
+      }
+
+    });
+
+    this.socket.fromEvent('message').subscribe(message => {
+      this.messages.push(message);
+      console.log('New: ',message);
+
+    });
+
+  }
+
+  sendMessage() {
+    this.socket.emit('send-message', {text: this.message});
+    this.message = '';
+  }
+
+  ionViewWillLeave() {
+    this.socket.disconnect();
+
+  }
+
+  async showToast(msg) {
+    let toast = await this.toastCtrl.create({
+      message: msg,
+      position: 'top',
+      duration: 2000
+    });
+    toast.present();
+  }
 
 }
